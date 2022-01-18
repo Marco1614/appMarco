32a906a720d03bc95f1529a8d1bf3c5f9a90f969
Tu nombre <marco.jarro1998@gmail.com>
2022-01-18 12:40:47 -0500

    git activo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { computeStackId } from '@ionic/angular/directives/navigation/stack-utils';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+      console.log("Abriendo mi aplicacion");
+      let num=10;
+      debugger;
+      num=20;
+      num=num+10;
+      num=1;
+      console.log(num);
+  }
 
 }
